062bfb100855df014afe6656a15db895b7f21682
Jianing-Ding <2391354814@qq.com>
2020-04-22 21:38:49 +0800

    更改了个人信息部分，当前代码还很不完善，先做测试

--- miniprogram/pages/myInfo/info.js
+++ miniprogram/pages/myInfo/info.js
@@ -37,6 +37,22 @@ Page({
               })
             }
           })
+        }else{
+          wx.login({
+            success (res) {
+              if (res.code) {
+                //发起网络请求
+                wx.request({
+                  url: 'https://test.com/onLogin',
+                  data: {
+                    code: res.code
+                  }
+                })
+              } else {
+                console.log('登录失败！' + res.errMsg)
+              }
+            }
+          })
         }
       }
     })
